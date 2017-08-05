39769e7942bedfdc7597184453c64e2e968217ce
tothutra <tothutra@gmail.com>
2017-08-05 16:34:59 -0400

    add client's views and method

--- app/models/client.rb
+++ app/models/client.rb
@@ -2,4 +2,16 @@ class Client < ApplicationRecord
   has_many :orders
   belongs_to :type
   has_many :redemptions, through: :orders
+
+  def client_type
+    self.type.name
+  end
+
+  def total_credits_ordered
+    total_credits_ordered = 0
+    self.orders.each do |order|
+      total_credits_ordered += order.total_credits
+    end
+    return total_credits_ordered
+  end
 end
--- app/serializers/order_serializer.rb
+++ app/serializers/order_serializer.rb
@@ -1,5 +1,5 @@
 class OrderSerializer < ActiveModel::Serializer
-  attributes :id, :order_id, :client_id, :date_created
+  attributes :id, :order_id, :client_id, :date_created, :total_credits
   has_many :redemptions
   belongs_to :client
 end
