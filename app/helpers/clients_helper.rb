e0b3736887ce41b0bfa5a2370961b1251eaea1ac
tothutra <tothutra@gmail.com>
2017-08-06 01:40:47 -0400

    refactor client helper

--- app/helpers/clients_helper.rb
+++ app/helpers/clients_helper.rb
@@ -1,2 +1,29 @@
 module ClientsHelper
+  def type_name(client)
+    client.type.name
+  end
+
+  def number_of_orders(client)
+    client.orders.length
+  end
+
+  def total_credits_ordered(client)
+    total_credits_ordered = 0
+    client.orders.each do |order|
+      total_credits_ordered += order.total_credits
+    end
+    return total_credits_ordered
+  end
+
+  def total_credits_redeemed(client)
+    total_credits_redeemed = 0
+    client.orders.each do |order|
+      total_credits_redeemed += order.credits_redeemed
+    end
+    return total_credits_redeemed
+  end
+
+  def total_credits_avail(client)
+    self.total_credits_ordered(client) - self.total_credits_redeemed(client)
+  end
 end
